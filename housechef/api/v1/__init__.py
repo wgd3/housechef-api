847087a11beac5be9ba806c1425872ee3d3d35ae
Wallace Daniel <wgd3@users.noreply.github.com>
2021-03-26 14:27:16 -0400

    moved api from subdomain to subfolder

--- housechef/api/v1/__init__.py
+++ housechef/api/v1/__init__.py
@@ -24,7 +24,7 @@ from .namespaces import (
     diet_type_ns,
 )
 
-api_v1 = Blueprint("api_v1", __name__, url_prefix="/v1", subdomain="api")
+api_v1 = Blueprint("api_v1", __name__, url_prefix="/api/v1")
 api = Api(
     api_v1,
     version="1.0",
